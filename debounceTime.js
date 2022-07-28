6cacb7aa726036b707f3386eea61fb1225bd42cf
andrey.ostroglyad <andrey.ostroglyad@hibob.io>
2022-07-28 16:47:42 +0300

    - debounceTime

---
+++ debounceTime.js
@@ -0,0 +1,30 @@
+// export const myDebounceTime = <T>(debounce: number) => (source: Observable<T>) => {
+//     return new Observable((observer: Observer<T>)) => {
+//         let timeoutId: number | undefined;
+//         let sourceCompleted = false;
+//         const subscription = source.subscribe(
+//             (next: T) => {
+//                 if(timeoutId) {
+//                     clearTimeout(timeoutId);
+//                 }
+//
+//                 timeoutId = setTimeout(() => {
+//                     observer.next(next);
+//                     timeoutId = undefined;
+//                     if(sourceCompleted) {
+//                         observer.complete();
+//                     }
+//                 }, debounce);
+//             },
+//             (err: any) => observer.error(err),
+//             () => {
+//                 sourceCompleted = true;
+//                 if(!timeoutId) {
+//                     observer.complete();
+//                 }
+//             }
+//         );
+//
+//         return subscription;
+//     }
+// }
